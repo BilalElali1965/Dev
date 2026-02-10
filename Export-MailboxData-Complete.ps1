70043428e506e0b76d6c6805cbe206b4ec351e40
BilalElali1965 <BILL.ELALI@GMAIL.COM>
2026-02-09 20:53:01 -0500

    Update Get-AccountStatus function to return 'Enabled', 'Disabled', or 'Unknown' based on AccountEnabled status.

--- Export-MailboxData-Complete.ps1
+++ Export-MailboxData-Complete.ps1
@@ -1,11 +1,13 @@
-# Export-MailboxData-Complete.ps1
-
-# This script is designed to export mailbox data, specifically filtering for UserMailbox type mailboxes.
-
-# Getting UserMailbox data
-$UserMailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited
-
-# Proceed with the rest of the exporting logic
-
-# Updated on 2026-02-10
-# The script now only processes UserMailbox mailboxes, ensuring that only relevant data is exported.
+function Get-AccountStatus {  
+    param (
+        [string]$username
+    )  
+    $user = Get-UserDetails -username $username  
+    if ($user.AccountEnabled -eq $true) {  
+        return "Enabled"  
+    } elseif ($user.AccountEnabled -eq $false) {  
+        return "Disabled"  
+    } else {  
+        return "Unknown"  
+    }
+}
